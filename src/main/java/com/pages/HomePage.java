209df1556e7ed134c35169a9828cb701704d54d2
Ganesh <kashidganesh243@gmail.com>
2024-08-13 01:06:47 +0530

    update code

--- src/main/java/com/pages/HomePage.java
+++ src/main/java/com/pages/HomePage.java
@@ -20,22 +20,14 @@ public class HomePage {
     private By productListLocator = By.cssSelector(".product_list");
     private By productItemLocator = By.cssSelector(".product_item");
 
-    /**
-     * Initializes the HomePage with a WebDriver instance.
-     *
-     * @param driver The WebDriver used to interact with the web page.
-     */
+
     public HomePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         this.actions = new Actions(driver);
     }
 
-    /**
-     * Performs a search for a given product keyword.
-     *
-     * @param keyword The product keyword to search for.
-     */
+
     public void searchForProduct(String keyword) {
         WebElement searchBox = driver.findElement(searchBoxLocator);
         searchBox.clear();
@@ -43,21 +35,13 @@ public class HomePage {
         driver.findElement(searchButtonLocator).click();
     }
 
-    /**
-     * Retrieves a list of products displayed on the homepage.
-     *
-     * @return A list of WebElements representing the products.
-     */
+
     public List<WebElement> getDisplayedProducts() {
         WebElement productList = driver.findElement(productListLocator);
         return productList.findElements(productItemLocator);
     }
 
-    /**
-     * Navigates to the details page of a specific product.
-     *
-     * @param productName The name of the product to navigate to.
-     */
+
     public void goToProductDetails(String productName) {
         List<WebElement> products = getDisplayedProducts();
         for (WebElement product : products) {
--- src/main/java/com/pages/ProductDetails.java
+++ src/main/java/com/pages/ProductDetails.java
@@ -11,54 +11,36 @@ public class ProductDetails {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    // Locators for product details elements
+
     private By productNameLocator = By.cssSelector(".product_title");
     private By productPriceLocator = By.cssSelector(".product_price");
     private By addToCartButtonLocator = By.id("add_to_cart");
     private By quantityFieldLocator = By.id("quantity_wanted");
 
-    /**
-     * Initializes the ProductDetails with a WebDriver instance.
-     *
-     * @param driver The WebDriver used to interact with the web page.
-     */
+
     public ProductDetails(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    /**
-     * Retrieves the name of the product.
-     *
-     * @return The product name as a String.
-     */
+
     public String getProductName() {
         return driver.findElement(productNameLocator).getText();
     }
 
-    /**
-     * Retrieves the price of the product.
-     *
-     * @return The product price as a String.
-     */
+
     public String getProductPrice() {
         return driver.findElement(productPriceLocator).getText();
     }
 
-    /**
-     * Sets the desired quantity for the product.
-     *
-     * @param quantity The quantity to set.
-     */
+
     public void setProductQuantity(int quantity) {
         WebElement quantityField = driver.findElement(quantityFieldLocator);
         quantityField.clear();
         quantityField.sendKeys(String.valueOf(quantity));
     }
 
-    /**
-     * Adds the product to the shopping cart.
-     */
+
     public void addToCart() {
         driver.findElement(addToCartButtonLocator).click();
     }
