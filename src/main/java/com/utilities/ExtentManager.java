ea9bea694cd477479654300b34082e8b2195dc65
Ganesh <kashidganesh243@gmail.com>
2024-08-26 15:53:47 +0530

    update code

--- src/main/java/com/utilities/ExtentManager.java
+++ src/main/java/com/utilities/ExtentManager.java
@@ -1,15 +1,18 @@
 package com.utilities;
 
 public class ExtentManager {
-    private static ExtentReports extent;
+//    public synchronized static ExtentReports getReporter() {
+//        if (extent == null) {
+//            ExtentSparkReporter sparkReporter = new ExtentSparkReporter("extentReport.html")
+//                    .viewConfigurer()
+//                    .viewOrder()
+//                    .as(new ViewName[] { ViewName.DASHBOARD, ViewName.TEST, ViewName.CATEGORY })
+//                    .apply();
+//            extent = new ExtentReports();
+//            extent.setSystemInfo("Test Environment", "Local");
+//            extent.attachReporter(sparkReporter);
+//        }
+//        return extent;
 
-    public synchronized static ExtentReports getReporter() {
-        if (extent == null) {
-            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("extentReport.html");
-            extent = new ExtentReports();
-            extent.attachReporter(htmlReporter);
-        }
-        return extent;
-    }
 
 }
