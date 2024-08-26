a9b7251a7cc20da2c683c05d211206d6f511aedf
Ganesh <kashidganesh243@gmail.com>
2024-08-26 15:51:36 +0530

    update code

--- src/main/java/com/utilities/TestListener.java
+++ src/main/java/com/utilities/TestListener.java
@@ -12,14 +12,15 @@ public class TestListener implements ITestListener {
     @Override
     public void onTestFailure(ITestResult result) {
         Object testClass = result.getInstance();
-        WebDriver driver = ((BaseTestMethod) testClass).getDriver();
+      //  WebDriver driver = ((BaseTestMethod) testClass).getDriver();
 
         // Capture and embed screenshot in the test report
-        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        TakesScreenshot TakesScreenshot = null;
+        TakesScreenshot takesScreenshot = (TakesScreenshot) ;
         String src = takesScreenshot.getScreenshotAs(OutputType.BASE64);
         // Utilize a reporting tool to embed the screenshot - Example for ExtentReports
-        ExtentTest test = ExtentManager.getReporter().createTest(result.getName());
-        test.addScreenCaptureFromBase64String(src, "Failure screenshot");
-        test.fail(result.getThrowable());
+        //ExtentTest test = ExtentManager.getReporter().createTest(result.getName());
+       // test.addScreenCaptureFromBase64String(src, "Failure screenshot");
+       // test.fail(result.getThrowable());
     }
 }
