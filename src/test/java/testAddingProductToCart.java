7f1da36658d8ae8d68363707e4812354806c6ef5
Ganesh <kashidganesh243@gmail.com>
2024-08-26 16:14:31 +0530

    update code

--- src/test/java/testAddingProductToCart.java
+++ src/test/java/testAddingProductToCart.java
@@ -12,7 +12,12 @@ public class testAddingProductToCart {
         ProductDetails productDetails = homePage.searchAndNavigateToProduct("88 BSLT Skis");
         productDetails.clickAddToCart();
         CartPage cartPage = new CartPage("","");
+    }
 
-
+    @Test
+    public void testAddMultipleProductsToCart() {
+        // Set up pages needed for the test
+        // Add necessary steps to navigate to and add multiple products
+        // Assert that all products are added to the cart with the correct quantities
     }
 }
